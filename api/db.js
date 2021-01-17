32e20a4c6f9cdf261de3af21882164a6d3ba2cbb
Siva <sivam1@netapp.com>
2021-01-17 18:53:03 +0530

    all movies is done

--- api/db.js
+++ api/db.js
@@ -1,14 +1,33 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
  
-const adapter = new FileSync('db.json')
+const adapter = new FileSync('db.json', {
+    defaultValue: {
+        movies: [
+            { title: 'asfd1', imageUrl: '', imdbRating: 4.5, genre: 'Comedy' },
+            { title: 'asfd2', imageUrl: '', imdbRating: 2.5, genre: 'Fantasy' },
+            { title: 'asfd3', imageUrl: '', imdbRating: 5.5, genre: 'Crime' },
+            { title: 'asfd4', imageUrl: '', imdbRating: 1.5, genre: 'Drama' },
+            { title: 'asfd5', imageUrl: '', imdbRating: 2.5, genre: 'Adventure' },
+            
+        ],
+        recentMovies: [
+
+        ],
+        recommendedMovies: [
+
+        ]
+    }
+});
 const db = low(adapter)
 
-// Set some defaults
-db.defaults({ movies: [],  })
-  .write()
+module.exports = db;
+
+// // Set some defaults
+// db.defaults({ movies: [],  })
+//   .write()
  
-// Add a post
-db.get('posts')
-  .push({ id: 1, title: 'lowdb is awesome'})
-  .write()
+// // Add a post
+// db.get('posts')
+//   .push({ id: 1, title: 'lowdb is awesome'})
+//   .write()
--- api/server.js
+++ api/server.js
@@ -1,21 +1,45 @@
 const express = require('express'), bodyParser = require('body-parser');
 const app = express()
 const port = 3000
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const signingSecret = 'some-secret';
+const username = 'test123';
+const password = 'password123';
+const token = jwt.sign({ username }, signingSecret);
+const db = require('./db');
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
 
 app.post('/login', (req, res) => {
-    if ( req.body.username === 'test123' && req.body.password === 'password123' ) {
-      var token = jwt.sign({ username: 'test123' }, 'some-secret');
-      res.send({token});
-    } else {
-      res.status(401);
-      res.send();
+  if ( req.body.username === username && req.body.password === password ) {
+    res.send({token});
+  } else {
+    res.status(401);
+    res.send();
+  }
+});
+
+app.use(function(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ error: 'No credentials sent!' });
+  } else {
+    try {
+      var token = jwt.verify(req.headers.authorization, signingSecret);
+      if ( token.username === username ) {
+        next();
+      } else {
+        return res.status(401).json({msg: 'Invalid Token'})
+      }
+    } catch( e ) {
+      return res.status(401).json({msg: 'Invalid Token'})
     }
+  }
+});
+
+
+app.get('/allmovies', (req, res) => {
+    const movies = db.get('movies').value();
+    res.send(movies)
 });
     
 
